1e1288516622133ea3420078d0e6153ed7c4be77
Sochukwuma Nwokoye <somanwokoye@gmail.com>
2025-04-16 12:55:23 +0100

    Delete src/clients_dev directory

--- src/clients_dev/tenants-react-web-client/src/global/app.settings.tsx
+++
@@ -1,2 +0,0 @@
-export const API_VERSION_URL = '/v1';
-
--- src/clients_dev/user-react-web-client/src/user-app/components/search/interfaces/search-result.interface.ts
+++
@@ -1,7 +0,0 @@
-export default interface UserSearchResult { //as in server side UserSearchBody
-    id: number,
-    firstName: string,
-    lastName: string,
-    homeAddress: string,
-    landlord?: boolean
-}
